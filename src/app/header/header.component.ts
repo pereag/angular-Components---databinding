2732c14800895c45d909e32d992bc982de869e0f
vapersmile <valentin.perello@smile.fr>
2024-04-30 17:33:56 +0200

    feat: add routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() navigationlinkEvent = new EventEmitter<string>();
 
+  navigationLinkHandle(link: string) {
+    console.log(link)
+    this.navigationlinkEvent.emit(link)
+  }
 }
+
+
